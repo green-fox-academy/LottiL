28fc4a97f7526961bcbfe97744f1705b35d2baa7
Lotti Lamm <lamm.lotti@gmail.com>
2021-11-25 21:47:15 +0100

    new files

---
+++ Week03/Day05/proba.ts
@@ -0,0 +1,3 @@
+console.log("Lotti Simon-Lamm")
+console.log("32")
+console.log("1.64")
---
+++ Week03/Day05/proba3.ts
@@ -0,0 +1,9 @@
+let hours = 6
+let weeks = 17
+let days = 5
+
+console.log(hours * weeks * days + ' hours')
+
+let work = 52
+
+console.log(hours * days / work * 100 + ' %')
