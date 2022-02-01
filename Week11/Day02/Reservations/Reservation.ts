0ecd65cc754e386588d520c57974fb9818c2e6a8
Lotti Lamm <lamm.lotti@gmail.com>
2022-02-01 21:27:14 +0100

    repair reservation

--- Week11/Day02/Reservations/Reservation.ts
+++ Week11/Day02/Reservations/Reservation.ts
@@ -11,13 +11,18 @@ export class Reservation implements Reservationy{
 
     getDowBooking(): string{
         let dayOfWeek:string[]=["MON","TUE","WED","THU","FRI","SAT","SUN"]
-        let randomDay:string= dayOfWeek[Math.floor(Math.random() * 7)];
+        let randomDay:string= dayOfWeek[Math.floor(Math.random() * dayOfWeek.length)];
 
         return randomDay;
     }
 
     getCodeBooking(): string{
-        let randomCode:string= Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 8).toUpperCase();
+        let characters:string[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+        let randomCode:string= ""
+        for (let i = 0; i < 8; i++) {
+            randomCode += characters[Math.floor(Math.random() * characters.length)];            
+        }
+        //let randomCode:string= Math.random().toString(36).replace(/[^0-9a-z]+/g, '').substring(0, 8).toUpperCase(); ami nem jó, azt eldobja, így kevesebb karakter
 
         return randomCode;
     }
