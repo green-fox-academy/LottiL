f4d0c29b06e0c07b98c753d35c3f4c7c281c4f27
Lotti Lamm <lamm.lotti@gmail.com>
2022-06-01 18:21:38 +0200

    almost finished

--- Week26/Todo/server.js
+++ Week26/Todo/server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/todos', (req, res) => {
-    const query = `SELECT completed, id, text FROM todos`;
+    const query = `SELECT completed, id, text FROM todos WHERE destroyed = false`;
     conn.query(query, (err, result) => {
         if (err) {
             console.error(err);
@@ -37,26 +37,20 @@ app.get('/api/todos', (req, res) => {
             return;
         }
 
-        const data = {
-            completed: result[0].completed,
-            id: result[0].id,
-            text: result[0].text
-        }
-
-        res.status(200).send(data);
+        res.status(200).send(result);
     });
 });
 
 app.get('/api/todos/:id', (req, res) => {
     const query = `SELECT SELECT completed, id, text FROM todos
     WHERE id = ?`;
-    const params = [req.params.id];
+    const params = [parseInt(req.params.id)];
     conn.query(query, params, (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).send({ message: err.sqlMessage });
             return;
-        } if (result.length <= 0) {
+        } if (result.length === 0 || parseInt(req.params.id) === NaN) {
             res.status(404).send({ message: "Not Found" });
             return
         }
@@ -77,35 +71,9 @@ app.post('/api/todos', (req, res) => {
             VALUES (?)
         `;
 
-        const params = [
-            req.body.text,
-        ];
-
-        conn.query(query, params, (err, result) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send({ message: err.sqlMessage });
-                return;
-            }
-            
-        const data = {
-            id: result.insertId,
-            text: req.body.text,
-            completed: req.body.completed
-            //bár szerintem mivel ez default, nem kell hogy benne legyen a body-ban
-        }
-            res.status(200).send(data);
-        });
-});
-
-app.put('/api/todos:id', (req, res) => {
-    const params = [req.params.id];
-
-    const query = `
-        UPDATE todos 
-        SET completed = true
-        WHERE id = ?
-    `;
+    const params = [
+        req.body.text,
+    ];
 
     conn.query(query, params, (err, result) => {
         if (err) {
@@ -113,18 +81,51 @@ app.put('/api/todos:id', (req, res) => {
             res.status(500).send({ message: err.sqlMessage });
             return;
         }
+
         const data = {
-            id: req.params.id,
+            id: result.insertId,
             text: req.body.text,
-            completed: req.body.completed
-            //bár szerintem mivel ez default, nem kell hogy benne legyen a body-ban, hanem itt csak a tru-t kéne beírni
+            completed: false
         }
         res.status(200).send(data);
     });
 });
 
-app.delete('/api/todos:id', (req, res) => {
-    const params = [req.params.id];
+app.put('/api/todos/:id', (req, res) => {
+    const query1 = 'SELECT * FROM todos WHERE id = ?';
+    const params1 = [parseInt(req.params.id)];
+
+    conn.query(query1, params1, (err1, rows) => {
+        if (rows.length === 0 || parseInt(req.params.id) === NaN) {
+            res.status(404).send('Not found');
+        }
+
+        const query2 = 'UPDATE todos SET text = ?, completed = ? WHERE id = ?';
+        const params2 = [
+            req.body.text || rows[0].text,
+            req.body.completed || rows[0].completed,
+            req.params.id,
+        ];
+
+        conn.query(query2, params2, (err2, result) => {
+            if (err2) {
+                console.error(err2);
+                res.status(500).send({ message: err2.sqlMessage });
+                return;
+            }
+            const data = {
+                id: req.params.id,
+                text: req.body.text || rows[0].text,
+                completed: req.body.completed || rows[0].completed
+            }
+            res.status(200).send(data);
+        });
+    });
+});
+    
+
+app.delete('/api/todos/:id', (req, res) => {
+    const params = [parseInt(req.params.id)];
 
     const query = `
         UPDATE todos 
@@ -137,12 +138,15 @@ app.delete('/api/todos:id', (req, res) => {
             console.error(err);
             res.status(500).send({ message: err.sqlMessage });
             return;
+        }if (result.length <= 0 || parseInt(req.params.id) === NaN) {
+            res.status(404).send({ message: "Not Found" });
+            return
         }
         const data = {
             id: req.params.id,
             text: req.body.text,
             completed: req.body.completed,
-            destroyed: "true"
+            destroyed: true
         }
         res.status(200).send(data);
     });
@@ -150,4 +154,8 @@ app.delete('/api/todos:id', (req, res) => {
 
 
 app.listen(port, () =>
-    console.log(`Server running at http://localhost:${port}`));
+    console.log(`Server running at http://localhost:${port}`)
+);
+
+/*     Filter todo's by keyword
+    Export todos in CSV */
--- Week26/Todo/todo.sql
+++ Week26/Todo/todo.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS todos;
 CREATE TABLE todos (
     id INT AUTO_INCREMENT,
     text TEXT NOT NUll,
-    completed ENUM('false','true') DEFAULT 'false',
-    destroyed ENUM('false','true') DEFAULT 'false',
+    completed boolean DEFAULT false,
+    destroyed boolean DEFAULT false,
     PRIMARY KEY (id)
 );
